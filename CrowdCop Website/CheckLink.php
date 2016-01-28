b2fc19474ceb8ef865cfa949650ae475dbc8954f
Austin Lubetkin <bocaaust@nugent-wl-39.dynamic.rpi.edu>
2016-01-28 17:53:16 -0500

    Linkchecker

---
+++ CrowdCop Website/CheckLink.php
@@ -0,0 +1,37 @@
+<?php
+
+       if (isDomainAvailible('http://www.css-tricks.com'))
+       {
+               echo "Up and running!";
+       }
+       else
+       {
+               echo "Woops, nothing found there.";
+       }
+
+       //returns true, if domain is availible, false if not
+       function isDomainAvailible($domain)
+       {
+               //check, if a valid url is provided
+               if(!filter_var($domain, FILTER_VALIDATE_URL))
+               {
+                       return false;
+               }
+
+               //initialize curl
+               $curlInit = curl_init($domain);
+               curl_setopt($curlInit,CURLOPT_CONNECTTIMEOUT,10);
+               curl_setopt($curlInit,CURLOPT_HEADER,true);
+               curl_setopt($curlInit,CURLOPT_NOBODY,true);
+               curl_setopt($curlInit,CURLOPT_RETURNTRANSFER,true);
+
+               //get answer
+               $response = curl_exec($curlInit);
+
+               curl_close($curlInit);
+
+               if ($response) return true;
+
+               return false;
+       }
+?>
