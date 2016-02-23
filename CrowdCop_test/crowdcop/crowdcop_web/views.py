dd734064aa1d7d2bac616f00ab17674a61e1b42d
kburk1997 <kburk97@optonline.net>
2016-02-23 16:19:32 -0500

    add django stuff

---
+++ CrowdCop_test/crowdcop/crowdcop_web/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+def index(request):
+	return render(request, 'crowdcop_web/index_template.html')
