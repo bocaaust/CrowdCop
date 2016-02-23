b1b28cf113e574d92c54416a15cca8e91ed8dba7
kburk1997 <kburk97@optonline.net>
2016-02-23 16:42:08 -0500

    more django stuff

--- CrowdCop_test/crowdcop/crowdcop_web/admin.py
+++ CrowdCop_test/crowdcop/crowdcop_web/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from models import Campaign
+
 # Register your models here.
+admin.site.register(Campaign)
--- CrowdCop_test/crowdcop/crowdcop_web/models.py
+++ CrowdCop_test/crowdcop/crowdcop_web/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Campaign(models.Model):
+	campaign_title = models.CharField(max_length=200)
+	start_date = models.DateTimeField('date published')
+	campaign_description = models.TextField()
+	amount_crowdfunded = models.DecimalField(max_digits=5, decimal_places=2)
+	campaign_image = models.ImageField()
