f3447292ed623558a0e549eff11cf70b8e6e3cba
kburk1997 <kburk97@optonline.net>
2016-03-01 17:22:53 -0500

    more models, add forloop counter

--- CrowdCop_test/crowdcop/crowdcop_web/models.py
+++ CrowdCop_test/crowdcop/crowdcop_web/models.py
@@ -14,6 +14,8 @@ class Campaign(models.Model):
 class User(models.Model):
 	firstname = models.CharField(max_length=20)
 	lastname = models.CharField(max_length=20)
+	joindate = models.DateTimeField('date joined')
+	
 
 class Contribution(models.Model):
 	
