6bbb08cf5959b261c1e475d961a9a1fa317b2851
kburk1997 <kburk97@optonline.net>
2016-03-08 16:24:45 -0500

    fix creator issues

--- CrowdCop_test/crowdcop/crowdcop_web/urls.py
+++ CrowdCop_test/crowdcop/crowdcop_web/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from . import views
 
+handler404 = 'crowdcop_web.views.error_404'
+
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
