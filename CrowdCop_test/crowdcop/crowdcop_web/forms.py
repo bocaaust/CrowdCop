e2413cd75999fb6343acf9ecc5de217f04e0eb9f
kburk1997 <kburk97@optonline.net>
2016-03-22 16:56:07 -0400

    profile stuff

--- CrowdCop_test/crowdcop/crowdcop_web/forms.py
+++ CrowdCop_test/crowdcop/crowdcop_web/forms.py
@@ -7,7 +7,7 @@ class UserForm(forms.ModelForm):
 
 	class Meta:
 		model = User
-		fields = ('username', 'email', 'password')
+		fields = ('username', 'email', 'first_name', 'last_name','password')
 
 class CrowdcopUserForm(forms.ModelForm):
 	class Meta:
