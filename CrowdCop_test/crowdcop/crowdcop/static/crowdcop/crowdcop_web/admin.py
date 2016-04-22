0bbcc530334d0b0b2a128fec5b24fc49f439ae63
Austin Lubetkin <bocaaust@gmail.com>
2016-04-22 04:22:53 -0400

    Fixed accidental duplication

--- CrowdCop_test/crowdcop/crowdcop/static/crowdcop/crowdcop_web/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from models import Campaign, CrowdcopUser, Tip, PayPalID
-
-# Register your models here.
-admin.site.register(Campaign)
-admin.site.register(CrowdcopUser)
-admin.site.register(Tip)
-admin.site.register(PayPalID)
