8881edfde88d4e49b090dbd5e4d6e5750d4b9a54
kburk1997 <kburk97@optonline.net>
2016-04-26 16:57:57 -0400

    attempt to fix trending bug

--- CrowdCop_test/crowdcop/crowdcop/urls.py
+++ CrowdCop_test/crowdcop/crowdcop/urls.py
@@ -30,3 +30,4 @@ urlpatterns = [
   url(r'^captcha/', include('captcha.urls')),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
